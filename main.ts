8a7e25dfa7b75475ae4baf00a167678c95750084
Abdul Rahman Dastagir <102039148+mdastagir72@users.noreply.github.com>
2022-08-04 22:04:20 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        kitronik_servo_lite.forward()
-    } else if (receivedNumber == 2) {
         kitronik_servo_lite.backward()
+    } else if (receivedNumber == 2) {
+        kitronik_servo_lite.forward()
     } else if (receivedNumber == 3) {
-        kitronik_servo_lite.turnRight(22.5)
+        kitronik_servo_lite.turnRight(3)
     } else if (receivedNumber == 4) {
-        kitronik_servo_lite.turnLeft(22.5)
+        kitronik_servo_lite.turnLeft(3)
     } else {
         kitronik_servo_lite.stop()
     }
 })
 radio.setGroup(100)
-basic.forever(function () {
-	
-})
